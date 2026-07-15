4d9ccbda7571cee41bf0ff2bafc499da2bb9f201
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-15 18:33:35 +0800

    Initial commit

---
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/GradeTrackerWebAppApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.gradetrackerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeTrackerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeTrackerWebAppApplication.class, args);
+	}
+
+}
