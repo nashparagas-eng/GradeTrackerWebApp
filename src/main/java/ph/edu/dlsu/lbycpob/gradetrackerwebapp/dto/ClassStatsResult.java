f4d15fb8a034f946e0afa2418753ad6aaec3a75d
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-16 12:39:11 +0800

    Added imports to ClassStatsResult

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/dto/ClassStatsResult.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/dto/ClassStatsResult.java
@@ -1,4 +1,17 @@
 package ph.edu.dlsu.lbycpob.gradetrackerwebapp.dto;
 
+// ============================================================
+// ClassStatsResult.java
+// ============================================================
 public class ClassStatsResult {
-}
+
+    private int    totalStudents;
+    private String highestName;
+    private double highestGrade;
+    private char   highestRank;
+    private String lowestName;
+    private double lowestGrade;
+    private char   lowestRank;
+    private double classMean;
+    private char   meanRank;
+
