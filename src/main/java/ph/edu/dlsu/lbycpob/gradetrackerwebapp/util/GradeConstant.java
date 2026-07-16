4e7879d9a5d76607de8f5e072711ceee3bbcc16a
12507393 <francis_martin_ang@dlsu.edu.ph>
2026-07-16 12:53:10 +0800

    added the annotations for the grade constants

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/util/GradeConstant.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/util/GradeConstant.java
@@ -1,4 +1,11 @@
-package ph.edu.dlsu.lbycpob.gradetrackerwebapp.util;
+package ph.edu.dlsu.lbycpob.gradetracker.util;
 
-public class GradeConstant {
-}
+// ============================================================
+// GradeConstants.java
+// NOTE: No changes required.
+//   All constants are static final and have no IO dependency.
+//   Referenced identically in the web layer:
+//     GradeConstants.MAX_STUDENTS, GradeConstants.LAB_WEIGHT, etc.
+// ============================================================
+public final class GradeConstants {
+
