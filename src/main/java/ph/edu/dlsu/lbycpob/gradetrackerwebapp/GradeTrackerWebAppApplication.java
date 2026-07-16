98a4d4d9b18d5ef4647d7979265bcfb36a4e409f
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-16 12:26:00 +0800

    Fixed main entry

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/GradeTrackerWebAppApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/GradeTrackerWebAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GradeTrackerWebApp {
+public class GradeTrackerWebAppApplication {
     static void main(String[] args) {
         SpringApplication.run(GradeTrackerWebAppApplication.class, args);
     }
