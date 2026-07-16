98819869415cf3ffff76231af9ebc5fe4d857167
12507393 <francis_martin_ang@dlsu.edu.ph>
2026-07-16 12:44:21 +0800

    added tthe student class variables inside it.

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/model/Student.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/model/Student.java
@@ -1,4 +1,11 @@
-package ph.edu.dlsu.lbycpob.gradetrackerwebapp.model;
-
+package ph.edu.dlsu.lbycpob.gradetracker.model;
+// ============================================================
+// Student.java
+// ============================================================
 public class Student {
-}
+
+    private String name;
+    private String idNumber;
+    private double rawGrade;
+    private String numericGrade;
+    private char   letterRank;
