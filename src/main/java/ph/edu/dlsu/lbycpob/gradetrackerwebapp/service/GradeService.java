2f25270048cbacd0f34bce299f57a12d3695bc12
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-16 12:44:24 +0800

    Added imports to GradeService

--- src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/service/GradeService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetrackerwebapp/service/GradeService.java
@@ -1,4 +1,11 @@
-package ph.edu.dlsu.lbycpob.gradetrackerwebapp.service;
+package ph.edu.dlsu.lbycpob.gradetracker.service;
 
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.gradetracker.dto.StudentFormDTO;
+import ph.edu.dlsu.lbycpob.gradetracker.model.Student;
+import ph.edu.dlsu.lbycpob.gradetracker.util.GradeCalculator;
+import ph.edu.dlsu.lbycpob.gradetracker.util.IDVerifier;
+
+@Service
 public class GradeService {
 }
